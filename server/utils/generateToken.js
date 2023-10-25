14d3287330870b0458d345dff0e0458156b20524
Edras Monterroso <108895582+EmonterrosoF@users.noreply.github.com>
2023-10-25 22:24:15 +0000

    add coment

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 
+// utilidad para generar el token para mantener la sesion del usuario o cliente
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
