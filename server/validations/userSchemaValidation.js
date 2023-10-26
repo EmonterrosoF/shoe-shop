df97b118edb6082fc9d79f6a07701638106d95a2
Edras Monterroso <108895582+EmonterrosoF@users.noreply.github.com>
2023-10-26 22:28:40 +0000

    add updateUserSchema in userSchema

--- server/validations/userSchemaValidation.js
+++ server/validations/userSchemaValidation.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const registerSchema = z.object({
+export const registerUserSchema = z.object({
   body: z.object({
     name: z.string(),
     email: z.string().email(),
@@ -8,9 +8,17 @@ export const registerSchema = z.object({
   }),
 });
 
-export const loginSchema = z.object({
+export const loginUserSchema = z.object({
   body: z.object({
     email: z.string().email(),
     password: z.string().min(6),
+  }),
+});
+
+export const updateUserSchema = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    email: z.string().email().optional(),
+    password: z.string().min(6).optional(),
   }),
 });
