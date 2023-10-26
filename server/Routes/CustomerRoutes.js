15c943f82b89c179a65a220fe48030205e755252
Edras <monterrosoedras@gmail.com>
2023-10-26 07:47:02 -0600

    add validate data in login and add login schema validation

--- server/Routes/CustomerRoutes.js
+++ server/Routes/CustomerRoutes.js
@@ -4,60 +4,67 @@ import { admin, protectedCustomer } from "../Middleware/AuthMiddleware.js";
 import generateToken from "../utils/generateToken.js";
 import Customer from "../Models/CustomerModel.js";
 import { ValidateData } from "../Middleware/validationDataMiddleware.js";
-import { registerSchema } from "../validations/userSchemaValidation.js";
+import {
+  loginSchema,
+  registerSchema,
+} from "../validations/userSchemaValidation.js";
 
 const router = express.Router();
 
 // LOGIN
-router.post("/login", async (req, res) => {
+router.post("/login", ValidateData(loginSchema), async (req, res) => {
   const { email, password } = req.body;
 
-  const customer = await Customer.findOne({ email });
+  try {
+    const customer = await Customer.findOne({ email });
 
-  if (customer && (await Customer.matchPassword(password))) {
-    res.json({
+    if (customer && (await Customer.matchPassword(password))) {
+      res.json({
+        _id: customer._id,
+        name: customer.name,
+        email: customer.email,
+        token: generateToken(customer._id),
+        createdAt: customer.createdAt,
+      });
+    } else {
+      res.status(401);
+      const error = new Error("Invalid Email or Password");
+      next(error);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// registrar cliente
+router.post("/", ValidateData(registerSchema), async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const customerExists = await Customer.findOne({ email });
+
+    if (customerExists) {
+      res.status(400);
+      const error = new Error("user already exists");
+      next(error);
+    }
+
+    const customer = await Customer.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
       _id: customer._id,
       name: customer.name,
       email: customer.email,
-      isAdmin: customer.isAdmin,
       token: generateToken(customer._id),
-      createdAt: customer.createdAt,
     });
-  } else {
-    res.status(401);
-    throw new Error("Invalid Email or Password");
+  } catch (error) {
+    console.log(error.message);
   }
-}
-);
-
-// registrar cliente
-router.post("/", ValidateData(registerSchema), async (error, req, res, next) => {
-
-  const { name, email, password } = req.body;
-
-  const customerExists = await Customer.findOne({ email });
-
-  if (customerExists) {
-    res.status(400);
-    throw new Error("customer already exists");
-  }
-
-  const customer = await Customer.create({
-    name,
-    email,
-    password,
-  });
-
-  res.status(201).json({
-    _id: customer._id,
-    name: customer.name,
-    email: customer.email,
-    token: generateToken(customer._id),
-  });
-
-}
-
-);
+});
 
 // PROFILE
 router.get(
