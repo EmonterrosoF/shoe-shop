11ee9be5ad1a8a19590deb9077f5947fcfe21cd1
Edras Monterroso <108895582+EmonterrosoF@users.noreply.github.com>
2023-10-27 22:26:01 +0000

    update product schema validation and user schema

--- server/validations/productSchemaValidation.js
+++ server/validations/productSchemaValidation.js
@@ -1,6 +1,39 @@
 import { z } from "zod";
 
+// para body
+export const reviewProductSchema = z.object({
+  rating: z.number(),
+  comment: z.string(),
+});
+
+// para body
+export const createProductSchema = z.object({
+  name: z.string(),
+  price: z.number(),
+  description: z.string(),
+  image: z.string(),
+  countInStock: z.number(),
+});
+
+// para body
+export const updateProductSchema = z.object({
+  name: z.string().optional(),
+  price: z.number().optional(),
+  description: z.string().optional(),
+  image: z.string().optional(),
+  countInStock: z.number().optional(),
+});
+
+// para querys
 export const queryProductSchema = z.object({
-  pageNumber: z.string().default("1"),
-  keyword: z.string().optional(),
+  pageNumber: z
+    .string()
+    .regex(new RegExp(/^[0-9]+$/))
+    .default("1"),
+  keyword: z.string().default(""),
+});
+
+// para params
+export const paramsProductSchema = z.object({
+  id: z.string().regex(new RegExp(/^[0-9a-fA-F]{24}$/)),
 });
--- server/validations/userSchemaValidation.js
+++ server/validations/userSchemaValidation.js
@@ -1,24 +1,21 @@
 import { z } from "zod";
 
+// para body
 export const registerUserSchema = z.object({
-  body: z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  }),
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
 });
 
+// para body
 export const loginUserSchema = z.object({
-  body: z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
-  }),
+  email: z.string().email(),
+  password: z.string().min(6),
 });
 
+// para body
 export const updateUserSchema = z.object({
-  body: z.object({
-    name: z.string().optional().optional(),
-    email: z.string().email().optional(),
-    password: z.string().min(6).optional(),
-  }),
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  password: z.string().min(6).optional(),
 });
