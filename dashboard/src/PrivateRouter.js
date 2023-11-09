f689deb17a96521f50c65a8c91fb6985919716ed
Edras <monterrosoedras@gmail.com>
2023-11-09 09:50:57 -0600

    update private routes for protect rol

--- dashboard/src/PrivateRouter.js
+++ dashboard/src/PrivateRouter.js
@@ -2,15 +2,20 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
 
-function PrivateRouter({ component: Component, ...rest }) {
+function PrivateRouter({ needAdmin = false, component: Component, ...rest }) {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
   return (
     <Route
       {...rest}
       component={(props) => {
-        if (userInfo && userInfo.isAdmin) {
+        if (userInfo?.isAdmin) {
           return <Component {...props} />;
+        } else if (userInfo && !needAdmin) {
+          return <Component {...props} />;
+        } else if (needAdmin) {
+          return <Redirect to={`/`} />;
         } else {
           return <Redirect to={`/login`} />;
         }
