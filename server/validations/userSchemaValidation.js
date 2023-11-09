2562d811546d271dc72970db50c2b180b3782a67
Edras <monterrosoedras@gmail.com>
2023-11-09 10:02:29 -0600

    update user schema validation

--- server/validations/userSchemaValidation.js
+++ server/validations/userSchemaValidation.js
@@ -2,20 +2,25 @@ import { z } from "zod";
 
 // para body
 export const registerUserSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string().min(6),
+  name: z.string().min(1, "El nombre es requerido"),
+  email: z.string().email("Email incorrecto"),
+  password: z
+    .string()
+    .min(6, "La contraseña tiene que tener un minimo de 6 caracteres"),
 });
 
 // para body
 export const loginUserSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
+  email: z.string().email("Email incorrecto"),
+  password: z.string().min(1, "La contraseña es requerida"),
 });
 
 // para body
 export const updateUserSchema = z.object({
   name: z.string().optional(),
-  email: z.string().email().optional(),
-  password: z.string().min(6).optional(),
+  email: z.string().email("Email incorrecto").optional(),
+  password: z
+    .string()
+    .min(6, "La contraseña tiene que tener un minimo de 6 caracteres")
+    .optional(),
 });
