39a0e6fbf7d8c96cecfb1820fe74ec55035c4829
Edras <monterrosoedras@gmail.com>
2023-11-09 09:52:51 -0600

    update product component

--- dashboard/src/components/products/Product.js
+++ dashboard/src/components/products/Product.js
@@ -8,7 +8,7 @@ const Product = (props) => {
   const dispatch = useDispatch();
 
   const deletehandler = (id) => {
-    if (window.confirm("Are you sure??")) {
+    if (window.confirm("¿Estas seguro?")) {
       dispatch(deleteProduct(id));
     }
   };
@@ -22,23 +22,33 @@ const Product = (props) => {
           </Link>
           <div className="info-wrap">
             <Link to="#" className="title text-truncate">
-              {product.name}
+              <span className="text-success">Producto:</span> {product.name}
             </Link>
-            <div className="price mb-2">Q{product.price}</div>
+            <Link to="#" className="title text-truncate">
+              <span className="text-success">Categoria:</span>{" "}
+              {product.category?.name}
+            </Link>
+            <Link to="#" className="title text-truncate">
+              <span className="text-success">Stock:</span>{" "}
+              {product.countInStock}
+            </Link>
+            <div className="price mb-2">
+              <span className="text-success">Precio:</span> Q{product.price}
+            </div>
             <div className="row">
               <Link
-                to={`/product/${product._id}/edit`}
+                to={`/product/${product._id}/edit?cat=${product.category?._id}`}
                 className="btn btn-sm btn-outline-success p-2 pb-3 col-md-6"
               >
                 <i className="fas fa-pen"></i>
               </Link>
-              <Link
+              <button
                 to="#"
                 onClick={() => deletehandler(product._id)}
                 className="btn btn-sm btn-outline-danger p-2 pb-3 col-md-6"
               >
                 <i className="fas fa-trash-alt"></i>
-              </Link>
+              </button>
             </div>
           </div>
         </div>
