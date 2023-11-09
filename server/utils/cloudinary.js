5216013e35ea1b4a0403b49816087bb66336402f
Edras <monterrosoedras@gmail.com>
2023-11-09 10:01:53 -0600

    add cloudinary

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from "cloudinary";
+import { CLOUD_NAME, API_KEY, API_SECRET } from "../config.js";
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET,
+});
